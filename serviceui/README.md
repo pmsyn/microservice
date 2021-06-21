71c83923be638105aadeccd626a43b6d08fa8b71
pengmenghseng <pengmengsheng@126.com>
2021-06-21 23:21:07 +0800

    update

--- serviceui/README.md
+++ serviceui/README.md
@@ -22,3 +22,9 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+#项目优化
+## 1.实现项目加载进度条
+### 1.1安装npogress 
+### 1.2在axios request中展示进度条 response中隐藏进度条 配置
--- serviceui/src/main.js
+++ serviceui/src/main.js
@@ -3,17 +3,24 @@ import axios from 'axios'
 import './plugins/element'
 import App from './App.vue'
 import router from './router/router'
+import NProgress from 'nprogress'
 import store from './store'
 import './assets/global.css'
+import 'nprogress/nprogress.css'
 
 //请求拦截器，在请求路径中加入Authoration参数
 axios.interceptors.request.use(config => {
   config.headers.Authoration = window.sessionStorage.getItem("token");
   config.baseURL='/api'
+  NProgress.start();//显示进度条
+  return config;
+})
+
+axios.interceptors.response.use(config=>{
+  NProgress.done();//隐藏进度条
   return config;
 })
 Vue.prototype.$http = axios;
-
 Vue.config.productionTip = false
 
 
